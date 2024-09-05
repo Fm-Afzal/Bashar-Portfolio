1696eedca6d6dcf3635eabcebb96439be0402b36
Afzal <fmafzalulbare@gmail.com>
2024-09-05 12:50:06 +0600

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,29 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  darkMode: 'selector',
+  content: ["./**/*.{html,js}"],
+  theme: {
+    extend: {
+
+      backgroundImage: {
+        'bg-white': "url('/assets/svg/bg.svg')",
+        'bg-dark': "url('/assets/svg/bg-dark.svg')",  
+      },
+      fontFamily: {
+        poppins: ["Poppins", "sans-serif"],
+        madeCanvas: ["MADE Canvas", "sans-serif"]
+      },
+      colors: {
+        'olive-light': '#576665',
+        'light-gray': '#ECECEC',
+        'red-light': '#FFD6D6',
+      },
+      fontSize: {
+        '21': '21px',
+        '10xl': '132px',
+      },
+    },
+  },
+  plugins: [],
+}
+
