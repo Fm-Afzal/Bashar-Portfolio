74167945aca80a658bfb2dbc411f2a57a966cd81
Afzal <fmafzalulbare@gmail.com>
2024-09-07 16:28:48 +0600

    testimonials page add

--- src/js/main.js
+++ src/js/main.js
@@ -5,8 +5,18 @@ $(function () {
     $(".toggleNav").on("click", function () {
         $("body").toggleClass('nav-expanded');
     })
-    
+
     $("header .checkbox").on("click", function () {
         $("html").toggleClass('dark');
     })
 })
+
+$(function () {
+    new Swiper(".testimonialSwiper", {
+        slidesPerView: "1",
+        navigation: {
+            nextEl: ".swiper-button-next",
+            prevEl: ".swiper-button-prev",
+        },
+    });
+});
